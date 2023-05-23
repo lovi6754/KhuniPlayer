c6d3873b40d783792d47054cd3b8cb98a43220f6
lovi6754 <133924252+lovi6754@users.noreply.github.com>
2023-05-23 16:14:12 +0530

    Update start.py

--- Bikash/utils/inline/start.py
+++ Bikash/utils/inline/start.py
@@ -28,7 +28,7 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [           
             InlineKeyboardButton(
-                text="📱 𝗢𝘄𝗻𝗲𝗿 📱", url=f"https://t.me/about_meeBacha"
+                text="📱 𝗢𝘄𝗻𝗲𝗿 📱", url=f"https://t.me/loveguruoo"
             )
         ],
      ]
@@ -58,7 +58,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [           
             InlineKeyboardButton(
-                text="📱 𝗢𝘄𝗻𝗲𝗿 📱", url=f"https://t.me/about_meeBacha"
+                text="📱 𝗢𝘄𝗻𝗲𝗿 📱", url=f"https://t.me/loveguruo"
             )
         ],
         [
