a7bdf6a499410aa0b2e3d1dc37436b3ec95d26a7
lovi6754 <133924252+lovi6754@users.noreply.github.com>
2023-05-23 13:27:29 +0530

    Update bot.py

--- Bikash/core/bot.py
+++ Bikash/core/bot.py
@@ -8,11 +8,11 @@ from ..logging import LOGGER
 
 
 
-class BikashBot(Client):
+class massomBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            "KhuniMusic",
+            "InnocentMusix",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
