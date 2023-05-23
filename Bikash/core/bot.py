3f39c8ba4f9cf624cec650ae514254c7b684889d
lovi6754 <133924252+lovi6754@users.noreply.github.com>
2023-05-23 15:07:22 +0530

    Update bot.py

--- Bikash/core/bot.py
+++ Bikash/core/bot.py
@@ -8,7 +8,7 @@ from ..logging import LOGGER
 
 
 
-class massomBot(Client):
+class BikashBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
