c1b05c9877b385f341765f7e10eaac86ec591208
lovi6754 <133924252+lovi6754@users.noreply.github.com>
2023-06-04 11:30:25 +0530

    Update owner.py

--- plugins/extra/owner.py
+++ plugins/extra/owner.py
@@ -12,24 +12,24 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
     & ~filters.edited & filters.group & ~filters.edited)
 async def owner(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://graph.org/file/185978d3d6c2beff97073.jpg",
-        caption=f"""🥀 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 𝐅𝐨𝐫 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐎𝐰𝐧𝐞𝐫 𝐎𝐟 𝗞𝗵𝘂𝗻𝗶 𝗦𝗲𝗿𝘃𝗲𝗿, 𝐈𝐟 𝐘𝐨𝐮 𝐖𝐚𝐧𝐭 𝐏𝐫𝐨𝐦𝐨𝐭𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬 𝐎𝐫 𝐎𝐭𝐡𝐞𝐫𝐬 𝐋𝐢𝐧𝐤 𝐓𝐡𝐞𝐧 [𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞](https://t.me/lovingfriendsforever) & 𝐂𝐥𝐢𝐜𝐤 𝐎𝐭𝐡𝐞𝐫'𝐬 𝐁𝐮𝐭𝐭𝐨𝐧 & 𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐎𝐫 𝐆𝐫𝐨𝐮𝐩.. 🥀 .""",
+        photo=f"https://telegra.ph//file/20d2fbed4cb4bbc586c8a.jpg",
+        caption=f"""🥀 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 𝐅𝐨𝐫 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐎𝐰𝐧𝐞𝐫 𝐎𝐟 𝗟𝗼𝘃𝗲𝗴𝘂𝗿𝘂 𝗦𝗲𝗿𝘃𝗲𝗿, 𝐈𝐟 𝐘𝐨𝐮 𝐖𝐚𝐧𝐭 𝐏𝐫𝐨𝐦𝐨𝐭𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬 𝐎𝐫 𝐎𝐭𝐡𝐞𝐫𝐬 𝐋𝐢𝐧𝐤 𝐓𝐡𝐞𝐧 [𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞](https://t.me/loveguruo) & 𝐂𝐥𝐢𝐜𝐤 𝐎𝐭𝐡𝐞𝐫'𝐬 𝐁𝐮𝐭𝐭𝐨𝐧 & 𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐎𝐫 𝐆𝐫𝐨𝐮𝐩.. 🥀 .""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🥀 𝗞𝗵𝘂𝗻𝗶 𝗦𝗲𝗿𝘃𝗲𝗿 🥀", url=f"https://t.me/lovingfriendsforever")
+                        "🥀 𝗟𝗼𝘃𝗲𝗴𝘂𝗿𝘂 𝗦𝗲𝗿𝘃𝗲𝗿 🥀", url=f"https://t.me/innocentop")
             ],          
             [
                     InlineKeyboardButton(
-                        "🥀 𝗠𝗿 𝗞𝗵𝘂𝗻𝗶 🥀", url=f"https://t.me/MrKhunii")
+                        "🥀 𝗠𝗿 𝗟𝗼𝘃𝗲𝗴𝘂𝗿𝘂 🥀", url=f"https://t.me/loveguruo")
                 ],
                 [
                     InlineKeyboardButton(
-                        "🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"https://t.me/Dil_se_dil_tak_01"
+                        "🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"https://t.me/innocentop"
                     ),
                     InlineKeyboardButton(
-                        "🥀 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🥀", url=f"https://t.me/Khuni_X_Villan_Server")
+                        "🥀 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🥀", url=f"https://t.me/loveguruoo")
                 ]
             ]
         ),
